4fe69fe24ef4575f187e921c1e4ca2d9e440eefa
dolapo47 <adeleyeadedolapomayowa@gmail.com>
2020-01-31 14:06:00 +0100

    create trip class

---
+++ src/com/gdb/Trip.java
@@ -0,0 +1,26 @@
+package com.gdb;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Trip {
+    private int id;
+    private int busId;
+    private String from;
+    private String to;
+    private double amount;
+    private Date time;
+    private ArrayList<Customer> passenger;
+    private boolean isCompleted;
+
+    public Trip(int id, int busId, String from, String to, double amount, Date time, ArrayList<Customer> passenger, boolean isCompleted) {
+        this.id = id;
+        this.busId = busId;
+        this.from = from;
+        this.to = to;
+        this.amount = amount;
+        this.time = time;
+        this.passenger = passenger;
+        this.isCompleted = isCompleted;
+    }
+}
