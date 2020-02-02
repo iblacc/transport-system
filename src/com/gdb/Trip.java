a8998cdf78e808bb48469d613557d58234ad74e1
Meralds-World <grace.chidumaga@gmail.com>
2020-02-02 17:11:16 +0100

    Create Trip class

--- src/com/gdb/Trip.java
+++ src/com/gdb/Trip.java
@@ -7,32 +7,48 @@ import java.util.Date;
 import java.lang.String;
 
 public class Trip {
-    private static int id;
-    private String busId;
+    private static int idCounter = 0;
+    private int id;
+    private Bus bus;
     private String from;
     private String to;
     private double amount;
-    private Date time = new Date();
+    private Date time;
     private ArrayList<Customer> passenger;
+    private ArrayList<Integer> availableSeats;
+    private HashMap<String, ArrayList<Integer>> seatOwners;
     private boolean isCompleted = false;
 
-    public Trip(String busId, String from, String to, double amount) {
-        this.busId = busId;
+    public Trip(Bus bus, String from, String to, double amount) {
+        this.bus = bus;
         this.from = from;
         this.to = to;
         this.amount = amount;
+        this.id = ++idCounter;
+        this.passenger = new ArrayList<>();
+        this.availableSeats = new ArrayList<Integer>();
+        this.seatOwners = new HashMap<>();
+        this.time = new Date();
     }
 
     public Date viewTime(){
         return this.time;
     }
 
-    public String getBusId() {
-        return busId;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getBusId(){
+        return bus.getId();
+    }
+
+    public int getId() {
+        return id;
     }
 
     public boolean completeTrip(){
         this.isCompleted = true;
         return this.isCompleted;
     }
-}
