9b28dcb2fecfdd111137e23769aaa147f3d57023
Meralds-World <grace.chidumaga@gmail.com>
2020-02-02 17:17:09 +0100

    Create Motor Station

---
+++ src/com/gdb/MotorStation.java
@@ -0,0 +1,22 @@
+package com.gdb;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.ListIterator;
+import java.util.Map;
+
+public class MotorStation {
+    ArrayList<Bus> buses;
+    ArrayList<Trip> completedTrips;
+    ArrayList<Trip> availableTrips;
+    ArrayList<User> users;
+    ArrayList<Complaints> complaints;
+
+
+    public MotorStation(){
+        this.buses = new ArrayList<>();
+        this.completedTrips = new ArrayList<>();
+        this.availableTrips = new ArrayList<>();
+        this.complaints = new ArrayList<>();
+        this.users = new ArrayList<>();
+    }
