ce3cfcc2ce2a3ff4596a7a22c5c7b4dd67fc2aa6
Meralds-World <grace.chidumaga@gmail.com>
2020-02-02 17:02:10 +0100

    create Customer class

---
+++ src/com/gdb/User.java
@@ -0,0 +1,30 @@
+package com.gdb;
+public abstract class User {
+    String name;
+    String email;
+    String phoneNumber;
+    String address;
+
+    public User(String name, String email, String phoneNumber, String address) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
